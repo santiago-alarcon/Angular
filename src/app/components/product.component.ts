0e0e5efdfa6f6b12ed1b972e71eaf2572c89c251
santiago <ajuliansantiago@gmil.com>
2020-05-30 19:11:22 -0500

    Nuevos cambios

--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -1,13 +1,27 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges, SimpleChange, OnInit } from '@angular/core';
 import {product} from '../product.model';
 @Component({
     selector: 'app-product',
     templateUrl: './product.component.html'
 })
 
-export class ProductComponent {
+export class ProductComponent implements OnChanges, OnInit{
 @Input() product: product;
 @Output() productClicked: EventEmitter<any> = new EventEmitter();
+
+constructor(){
+    console.log('constructor');
+}
+
+ngOnChange(changes: SimpleChange){
+    console.log('ngOnChanges');
+    console.log(changes);
+}
+
+ngOnInit (){
+    console.log('3.ngOnChanges')
+}
+
 
 addCart(){
 console.log('Añadir al carrito');
